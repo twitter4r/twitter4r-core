e79dc599216bf3dd77aeffa23d1f8917ba50ebc9
Susan Potter <me@susanpotter.net>
2010-08-07 12:28:20 -0500

    Added SVN snapshot version migration script.

---
+++ bin/migrate-svn-snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+TARGET_DIR=$2
+VERSION=$3
+GIT_COMMITTER_DATE=$4
+
+pushd ${SOURCE_DIR}
+git checkout rel_${VERSION}
+popd
+
+pushd ${TARGET_DIR}
+git checkout clean
+git branch v${VERSION}
+git checkout v${VERSION}
+popd
+
+cp -r ${SOURCE_DIR}/* ${TARGET_DIR}
+rm -rf ${TARGET_DIR}/{web,marketing} ${TARGET_DIR}/config/twitter.yml
+
+pushd ${TARGET_DIR}
+git add .
+git commit -m "Added v ${VERSION} snapshot."
+git tag -s twitter4r-v${VERSION} -m "Tagged version ${VERSION}"
+git checkout utilities
+popd
