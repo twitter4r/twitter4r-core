f2bebb264ca41f2467d614f5d3ede5f62d3134aa
Susan Potter <me@susanpotter.net>
2010-09-07 00:27:38 -0500

    Fixed some sanity test logic run before releasing.

--- scripts/testsanity.rb
+++ scripts/testsanity.rb
@@ -14,60 +14,60 @@ def expect(n, m, message = 'A potential error')
 end
 
 puts "Public timeline sanity check"
-timeline = twitter.timeline_for(:public)
+timeline = twitter.timeline_for(:public, :include_rts => 1)
 count = timeline.size
 expect 20, count, "Retrieved the last #{count} statuses from the public timeline"
 
-sleep(5)
+sleep(2)
 puts "Friends timeline sanity check"
-timeline = twitter.timeline_for(:friends)
+timeline = twitter.timeline_for(:friends, :include_rts => 1)
 count = timeline.size
 expect 20, count, "Retrieved the last #{count} statuses from all my friends' timeline"
 
-sleep(5)
+sleep(2)
 puts "User timeline sanity check"
-timeline = twitter.timeline_for(:user, :id => 'mbbx6spp', :per_page => 5)
+timeline = twitter.timeline_for(:user, :id => 'mbbx6spp', :count => 5, :include_rts => 1)
 count = timeline.size
 expect 5, count, "Retrieved the last #{count} statuses from one friend"
 
-sleep(5)
+sleep(2)
 puts "User lookup sanity check"
 screen_name = 'mbbx6spp'
 user = twitter.user(screen_name, :info)
 expect screen_name, user.screen_name, 'Retrieved a different user'
 
-sleep(5)
+sleep(2)
 puts "User#friends sanity check"
 friends = twitter.user(screen_name, :friends)
 expect Array, friends.class, 
 	'Did not retrieve an Array of users for #user(..., :friends)'
 
-sleep(5)
+sleep(2)
 puts "My user info sanity check"
 followers = twitter.my(:info).followers
 expect Array, followers.class, 
 	'Did not retrieve an Array of users for #my(:followers)'
 
-sleep(5)
+sleep(2)
 puts "Status posting sanity check"
 posted_status = twitter.status(:post, "Testing Twitter4R v#{version} - http://twitter4r.rubyforge.org")
-timeline = twitter.timeline_for(:me, :per_page => 1)
+timeline = twitter.timeline_for(:me, :count => 1)
 expect Twitter::Status, posted_status.class, 'Did not return newly posted status'
 expect 1, timeline.size, 'Did not retrieve only last status'
 expect Array, timeline.class, 'Did not retrieve an Array'
 expect timeline[0], posted_status, 'Did not successfully post new status'
 
-sleep(5)
+sleep(2)
 puts "Status retrieval sanity check"
 status = twitter.status(:get, posted_status)
 expect posted_status, status, 'Did not get proper status'
 
-sleep(5)
+sleep(2)
 puts "Status deletion sanity check"
 deleted_status = twitter.status(:delete, posted_status)
-expect posted_status, deleted_status, 'Did not delete same status'
+expect posted_status.id, deleted_status.id, 'Did not delete same status'
 
-sleep(5)
+sleep(2)
 puts "Direct messaging sanity check"
 text = 'This is a test direct message for sanity test script purposes'
 message = twitter.message(:post, text, user)
@@ -76,12 +76,12 @@ expect text, message.text,
 expect user.screen_name, message.recipient.screen_name, 
 	'Did not post to expected recipient'
 
-sleep(5)
+sleep(2)
 puts "Favorites sanity check"
 favorites = twitter.favorites
 expect 0, favorites.size, 'Did not receive expected number of favorites'
 
-sleep(5)
+sleep(2)
 puts "Add favorite sanity check"
 status_id = 381975342
 status = twitter.favorite(:add, status_id)
@@ -89,7 +89,7 @@ favorites = twitter.favorites
 expect 1, favorites.size, 'Did not add favorite'
 expect status_id, status.id, 'Did not add correct favorite'
 
-sleep(5)
+sleep(2)
 puts "Remove favorite sanity check"
 status = twitter.favorite(:remove, status_id)
 favorites = twitter.favorites
