5c34caf716ccf8353653c082259389e27305e689
Susan Potter <me@susanpotter.net>
2012-03-04 15:03:42 -0600

    Up version number to next release 0.8.0

--- lib/twitter/version.rb
+++ lib/twitter/version.rb
@@ -3,14 +3,14 @@
 
 module Twitter::Version #:nodoc:
   MAJOR = 0
-  MINOR = 7
-  REVISION = 1
+  MINOR = 8
+  REVISION = 0
   class << self
     # Returns X.Y.Z formatted version string
     def to_version
       "#{MAJOR}.#{MINOR}.#{REVISION}"
     end
-    
+
     # Returns X-Y-Z formatted version name
     def to_name
       "#{MAJOR}_#{MINOR}_#{REVISION}"
