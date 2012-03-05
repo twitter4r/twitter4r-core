2bc46dc3989f37633ddf8ed8a0f79584cc372a28
Susan Potter <me@susanpotter.net>
2012-03-04 22:12:14 -0600

    Change defaults for "housekeeping" scheduled 2012-03-14
    Twitter has mentioned deprecations and non-versioned endpoints will be
    terminated mid-March 2012.

--- lib/twitter/config.rb
+++ lib/twitter/config.rb
@@ -1,15 +1,15 @@
-# config.rb contains classes, methods and extends existing Twitter4R classes 
+# config.rb contains classes, methods and extends existing Twitter4R classes
 # to provide easy configuration facilities.
 
 module Twitter
   # Represents global configuration for Twitter::Client.
   # Can override the following configuration options:
   # * <tt>protocol</tt> - <tt>:http</tt>, <tt>:https</tt> or <tt>:ssl</tt> supported.  <tt>:ssl</tt> is an alias for <tt>:https</tt>.  Defaults to <tt>:ssl</tt>
-  # * <tt>host</tt> - hostname to connect to for the Twitter service.  Defaults to <tt>'twitter.com'</tt>.
+  # * <tt>host</tt> - hostname to connect to for the Twitter service.  Defaults to <tt>'api.twitter.com'</tt>.
   # * <tt>port</tt> - port to connect to for the Twitter service.  Defaults to <tt>443</tt>.
-  # * <tt>path_prefix</tt> - path to prefix URIs of REST API calls.  Defaults to <tt>""</tt>.
+  # * <tt>path_prefix</tt> - path to prefix URIs of REST API calls.  Defaults to <tt>"1"</tt>.
   # * <tt>search_protocol</tt> - <tt>:http</tt>, <tt>:https</tt> or <tt>:ssl</tt> supported.  <tt>:ssl</tt> is an alias for <tt>:https</tt>.  Defaults to <tt>:ssl</tt>
-  # * <tt>search_host</tt> - hostname to connect to for the Twitter Search service.  Defaults to <tt>'twitter.com'</tt>.
+  # * <tt>search_host</tt> - hostname to connect to for the Twitter Search service.  Defaults to <tt>'search.twitter.com'</tt>.
   # * <tt>search_port</tt> - port to connect to for the Twitter Search service.  Defaults to <tt>443</tt>.
   # * <tt>search_path_prefix</tt> - path to prefix URIs of Search API calls.  Defaults to <tt>""</tt>.
   # * <tt>proxy_protocol</tt> - proxy protocol to use.  Defaults to http.
@@ -35,9 +35,9 @@ module Twitter
   class Config
     include ClassUtilMixin
     @@ATTRIBUTES = [
-      :protocol, 
-      :host, 
-      :port, 
+      :protocol,
+      :host,
+      :port,
       :path_prefix,
       :search_protocol,
       :search_host,
@@ -67,8 +67,8 @@ module Twitter
     ]
 
     attr_accessor(*@@ATTRIBUTES)
-    
-    # Override of Object#eql? to ensure RSpec specifications run 
+
+    # Override of Object#eql? to ensure RSpec specifications run
     # correctly. Also done to follow Ruby best practices.
     def eql?(other)
       return true if self == other
@@ -80,13 +80,13 @@ module Twitter
   end
 
   class Client
-    @@defaults = { :host => 'twitter.com', 
-                   :port => 443, 
+    @@defaults = { :host => 'api.twitter.com',
+                   :port => 443,
                    :protocol => :ssl,
-                   :path_prefix => "",
+                   :path_prefix => "1",
                    :search_host => 'search.twitter.com',
-                   :search_port => 80,
-                   :search_protocol => :http,
+                   :search_port => 443,
+                   :search_protocol => :ssl,
                    :search_path_prefix => "",
                    :proxy_protocol => "http",
                    :proxy_host => nil,
@@ -94,7 +94,7 @@ module Twitter
                    :media_host => 'upload.twitter.com',
                    :media_port => 443,
                    :media_protocol => :ssl,
-                   :media_path_prefix => "",
+                   :media_path_prefix => "1",
                    :user_agent => "default",
                    :application_name => 'Twitter4R',
                    :application_version => Twitter::Version.to_version,
@@ -120,6 +120,6 @@ module Twitter
         raise ArgumentError, "Block must be provided to configure" unless block_given?
         yield config
       end # configure
-    end # class << self    
+    end # class << self
   end # Client class
 end # Twitter module
